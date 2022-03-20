eeb97f1d31f6ed73e577b9cf9136ca5961655ee5
makck <joel.mak32@gmail.com>
2022-03-21 00:37:13 +0800

    Create models/index.mjs

---
+++ models/index.mjs
@@ -0,0 +1,28 @@
+import sequelizePackage from 'sequelize';
+import allConfig from '../config/config.js';
+
+import initUserModel from './user.mjs';
+import initGameModel from './game.mjs';
+
+const { Sequelize } = sequelizePackage;
+const env = process.env.NODE_ENV || 'development';
+const config = allConfig[env];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config,
+);
+
+db.User = initUserModel(sequelize, Sequelize.DataTypes);
+db.Game = initGameModel(sequelize, Sequelize.DataTypes);
+
+db.User.belongsToMany(db.Game, { through: 'games_users' });
+db.Game.belongsToMany(db.User, { through: 'games_users' });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
